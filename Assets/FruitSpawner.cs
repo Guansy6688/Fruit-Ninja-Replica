dd176dca9cce0052c47c32b4ee6399fcfc53e8e8
Shenyong Guan <sguan@cae.com>
2017-09-12 14:04:13 -0400

    First commit

---
+++ Assets/FruitSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour {
+
+    public GameObject fruitPrefab;
+    public Transform[] spawnPoints;
+
+    public float minDelay = 0.1f;
+    public float maxDelay = 1f;
+
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnFruits());
+		
+	}
+	
+	IEnumerator SpawnFruits()
+    {
+        while (true)
+        {
+            float delay = Random.Range(minDelay, maxDelay);
+            yield return new WaitForSeconds(1f);
+            int spawnIndex = Random.Range(0, spawnPoints.Length);
+            Transform spawnPoint = spawnPoints[spawnIndex];
+
+            GameObject spawnedFruit = Instantiate(fruitPrefab, spawnPoint.position, spawnPoint.rotation);
+            Destroy(spawnedFruit, 2f);
+       
+        }
+    }
+}
